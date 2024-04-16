db97edd06e80eb21e689fe02ed58b03dc396a542
Nachtje <datanerd07@gmail.com>
2024-04-16 17:02:15 +0200

    Question 5

--- sql/nathalie.sql
+++ sql/nathalie.sql
@@ -76,3 +76,41 @@ JOIN
     Dim_countries dc ON fv.fk_country_id = dc.country_code
 GROUP BY
     dc.country_name, fv.year;
+
+--- Question 5
+---- !!!! Work in progress, queries are ok, but need refactoring with dim/fact tables
+--- first create a view to fetch top3 grape names
+CREATE VIEW top3_grape_names AS
+WITH top3grapes as (
+	SELECT 
+		mugpc.grape_id,
+		g.name, 
+		mugpc.wines_count
+	FROM most_used_grapes_per_country mugpc
+	join grapes g on g.id = mugpc.grape_id 
+	group by grape_id
+	order by wines_count desc
+	LIMIT 3
+	)
+SELECT name from top3grapes;
+
+
+--- next use the view joined with the wines to fetch top5 ranked wines
+SELECT
+	grape_name,
+    wine_name,
+    rating
+FROM
+    (
+        SELECT
+            g.name AS grape_name,
+            w.name AS wine_name,
+            w.ratings_average AS rating,
+            ROW_NUMBER() OVER(PARTITION BY g.name ORDER BY w.ratings_average DESC) AS row_num
+        FROM
+            top3_grape_names g
+        JOIN
+            wines w ON w.name LIKE '%' || g.name || '%'
+    ) AS ranked_wines
+WHERE
+    row_num <= 5;
