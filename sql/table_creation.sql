be24dea02141e34546f6038da1371e3c1de2cd4b
Nachtje <datanerd07@gmail.com>
2024-04-17 11:40:21 +0200

    updating creation/population all dims/facts for Q1, Q5, Q6

--- sql/table_creation.sql
+++ sql/table_creation.sql
@@ -1,9 +1,10 @@
--- Dimension Tables
+-- Dimension table Wines Q1, Q5, Q6
 CREATE TABLE Dim_wines (
   wine_id INTEGER PRIMARY KEY,
   wine_name VARCHAR
 );
 
+-- Dimension table Regions Q6
 CREATE TABLE Dim_regions (
   region_id INTEGER PRIMARY KEY,
   fk_country_id INTEGER,
@@ -11,12 +12,25 @@ CREATE TABLE Dim_regions (
   FOREIGN KEY (fk_country_id) REFERENCES Dim_countries(country_code)
 );
 
+-- Dimension table Countries Q6
 CREATE TABLE Dim_countries (
   country_code VARCHAR PRIMARY KEY,
   country_name VARCHAR
 );
 
--- Fact Table
+-- Dimension table Vintages Q6
+CREATE TABLE Dim_vintages (
+  vintages_id INTEGER PRIMARY KEY,
+  vintages_name VARCHAR
+);
+
+-- Dimension table Grapes Q5
+CREATE TABLE Dim_grapes (
+  grape_id INTEGER PRIMARY KEY,
+  grape_name VARCHAR  
+);
+
+-- Fact Table Wines Q1, Q6
 CREATE TABLE Fact_wines (
   fk_wine_id INTEGER,
   fk_region_id INTEGER,
@@ -30,12 +44,23 @@ CREATE TABLE Fact_wines (
   FOREIGN KEY (fk_country_id) REFERENCES Dim_countries(country_id)
 );
 
-CREATE TABLE Dim_grapes (
-  grape_id INTEGER PRIMARY KEY,
-  grape_name VARCHAR  
+-- Fact Table Vintages Q6
+CREATE TABLE Fact_vintages (
+  fk_vintages_id INTEGER,
+  fk_wine_id INTEGER,
+  fk_region_id INTEGER,
+  fk_country_id INTEGER,
+  ratings_avg INTEGER,
+  ratings_count INTEGER,
+  year INTEGER,
+  PRIMARY KEY (fk_vintages_id),
+  FOREIGN KEY (fk_vintages_id) REFERENCES Dim_vintages(vintages_id),
+  FOREIGN KEY (fk_wine_id) REFERENCES Dim_wines(wine_id),
+  FOREIGN KEY (fk_region_id) REFERENCES Dim_regions(region_id),
+  FOREIGN KEY (fk_country_id) REFERENCES Dim_countries(country_id)
 );
 
--- Fact Table
+-- Fact Table Grapes Q5
 CREATE TABLE Fact_grapes (
   fk_grape_id INTEGER,
   wines_count INTEGER,
--- sql/table_population.sql
+++ sql/table_population.sql
@@ -1,14 +1,23 @@
-INSERT INTO Dim_countries (country_code, country_name)
-SELECT DISTINCT code, name
-FROM countries;
+INSERT INTO Dim_wines (wine_id, wine_name)
+SELECT DISTINCT id, name
+FROM wines ;
 
 INSERT INTO Dim_regions (region_id, fk_country_id, region_name)
 SELECT DISTINCT id, country_code, name
 FROM regions;
 
-INSERT INTO Dim_wines (wine_id, wine_name)
+INSERT INTO Dim_countries (country_code, country_name)
+SELECT DISTINCT code, name
+FROM countries;
+
+INSERT INTO Dim_vintages (vintages_id, vintages_name)
 SELECT DISTINCT id, name
-FROM wines ;
+FROM vintages ;
+
+INSERT INTO Dim_grapes (grape_id, grape_name)
+SELECT DISTINCT id, name
+FROM grapes;  
+
 
 INSERT INTO Fact_wines (fk_wine_id, fk_region_id, fk_country_id, ratings_avg, ratings_count, calc_avg_price, calc_weighted_rating)
 WITH avg_prices AS(  
@@ -30,13 +39,26 @@ JOIN
     regions r ON w.region_id = r.id,
     avg_prices a ON w.id = a.wine_id;
 
-INSERT INTO Dim_grapes (grape_id, grape_name)
-SELECT DISTINCT id, name
-FROM grapes;  
+
+INSERT INTO Fact_vintages (fk_vintages_id, fk_wine_id, fk_region_id, fk_country_id, ratings_avg, ratings_count, year)
+SELECT
+    v.id,
+    v.wine_id,
+    w.region_id,
+    r.country_code,
+    v.ratings_average,
+    v.ratings_count,
+    v.year
+ 
+FROM
+    vintages v
+JOIN
+	wines w ON w.id = v.wine_id,
+	regions r ON w.region_id = r.id
+
 
 INSERT INTO Fact_grapes (fk_grape_id, wines_count)
 SELECT grape_id, wines_count
 FROM most_used_grapes_per_country mugpc
 GROUP BY grape_id 
    
-
