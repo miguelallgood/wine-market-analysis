21e2f6db8a345fca7f6ed0e025ae99493b833e51
Nachtje <datanerd07@gmail.com>
2024-04-18 13:03:39 +0200

    pages example

---
+++ src/pages/Q1.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+import pandas as pd
+import os
+
+# Configure the page
+st.set_page_config(
+    page_title="Wine Dashboard",
+    page_icon="🏡",
+)
+
+#####Sidebar Start#####
+
+# Add a sidebar
+st.sidebar.markdown("### **Q1**")
+
