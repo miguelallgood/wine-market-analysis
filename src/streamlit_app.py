585393011b486ce5d17df065b2c1ed0febee75c0
Andrea Haritcalde <andrea.haritcalde@gmail.com>
2024-04-19 13:31:53 +0200

    Add line in view at manage-db add q1,q2,q4

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -3,58 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from utils.get_results import award_best_wineries, recommend_cabernet_sauvignon, select_common_grapes_wines, \
-    highlight_top_wines_1, highlight_top_wines_2
+    highlight_top_wines_1, highlight_top_wines_2, prioritize_country, find_wines_by_taste
 
-# Dummy Data Preparation
-# You can replace these with actual database queries or more complex data structures as needed.
-top_10_wines = pd.DataFrame({
-    "Wine Name": ["Chateau Margaux", "Silver Oak Cabernet", "Caymus Cabernet", "Chardonnay Premiere", "Merlot Magic"],
-    "Ratings Average": [4.5, 4.3, 4.6, 4.2, 4.1],
-    "Price": [350, 220, 185, 90, 75],
-    "Country": ["France", "USA", "USA", "USA", "France"],
-    "Selection Reason": ["High rating & popularity", "Best seller in USA", "Excellent reviews", "Top choice for events", "Affordable luxury"]
-})
 
-marketing_priorities = pd.DataFrame({
-    "Country": ["Italy", "Spain", "France", "USA", "Australia"],
-    "Average Rating": [4.2, 4.0, 4.5, 4.1, 4.3],
-    "Number of Wineries": [150, 120, 130, 160, 110],
-    "Average Price": [30, 25, 45, 35, 28],
-    "Marketing Score": [88, 85, 90, 87, 86]
-})
 
-keyword_wines = pd.DataFrame({
-    "Wine Name": ["Morning Fog Chardonnay", "Dark Horse Cabernet"],
-    "Matched Keywords": [["cream", "citrus"], ["coffee", "toast"]],
-    "User Confirmations": [12, 15],
-    "Country": ["USA", "USA"]
-})
 
-common_grapes = pd.DataFrame({
-    "Grape": ["Chardonnay", "Merlot"],
-    "Wine Name": ["Sonoma Reserve", "Velvet Devil"],
-    "Ratings Average": [4.1, 4.0],
-    "Country": ["France", "USA"],
-    "Availability Score": [95, 90]
-})
 
-country_ratings = pd.DataFrame({
-    "Country": ["France", "Italy", "Spain"],
-    "Average Rating": [4.5, 4.3, 4.1]
-})
+header_style = '''
+    <style>
+        thead {
+            
+            background-color: rgb(255, 75, 75, 0.3);           
+            
+        }
+    </style>
 
-vintage_ratings = pd.DataFrame({
-    "Year": [1990, 2000, 2010],
-    "Average Rating": [4.2, 4.4, 4.6]
-})
-
-vip_recommendations = pd.DataFrame({
-    "Wine Name": ["Opus One", "Screaming Eagle"],
-    "Vintage Year": [2015, 2012],
-    "Country": ["USA", "USA"],
-    "Rating": [4.7, 4.9],
-    "Tasting Note": ["Rich and robust with a velvety texture", "Elegant and intense, notes of blackberry and spice"]
-})
+'''
+st.markdown(header_style, unsafe_allow_html=True)
 
 # Streamlit App Layout
 st.title('Wine Dashboard')
@@ -71,44 +36,115 @@ view = st.sidebar.selectbox("Choose a view", [
 ])
 
 if view == "Top 10 Wines":
-    df1 = highlight_top_wines_1()
-    df2 = highlight_top_wines_2()
-    formatted_df1 = df1.style.format({
-        "measure": "{:.0f}".format,
-        "avg_rating": "{:.1f}".format,
-        "average_weighted_price": "{:.0f}".format,
+    st.header("Top 10 Wines to Increase Sales")
+    st.write('Indicator is a measure that encapsulates: popularity, quality and vintage variety of a wine.')
+    st.write('This approach is for new customers: it highlights wines that are popular and have a good revenue.')
+    df1 = highlight_top_wines_1()      
+
+    formatted_df1 = df1.rename(columns={
+        "id": "Id",
+        "name": "Wine", 
+        "measure": "Indicator", 
+        "total_count": "Number Reviews",
+        "total_sale": "Estimated Revenue (€)",
+        "avg_rating": "Avg Rating", 
+        "average_weighted_price": "Avg Price (€)",
+        "numb_vintages": "Number Vintages"
+    })
+    formatted_df1 = formatted_df1.style.format({
+        "Indicator": "{:.0f}".format,
+        "Avg Rating": "{:.1f}".format,
+        "Avg Price (€)": "{:.0f}".format,
+        "Estimated Revenue (€)": "{:.0f}".format
     })
     st.table(formatted_df1)
-    st.table(df2)
-    # st.header("Top 10 Wines to Increase Sales")
-    # st.table(top_10_wines)
-    # fig, ax = plt.subplots()
-    # ax.bar(top_10_wines["Wine Name"], top_10_wines["Ratings Average"], color='skyblue')
-    # plt.xticks(rotation=45, ha="right")
-    # plt.xlabel("Wine Name")
-    # plt.ylabel("Ratings Average")
-    # st.pyplot(fig)
+    
+    df2 = highlight_top_wines_2()
+    formatted_df2 = df2.rename(columns={
+        "wine_id": "Id",
+        "wine_name": "Wine", 
+        "type_wine": "Type",
+        "total_count": "Number Reviews",
+        "total_sale": "Estimated Revenue (€)",
+        "avg_rating": "Avg Rating", 
+        "average_weighted_price": "Avg Price (€)",
+        "numb_vintages": "Number Vintages"
+    })
+    formatted_df2 = formatted_df2.style.format({
+        "Avg Rating": "{:.1f}".format,
+        "Avg Price (€)": "{:.0f}".format,
+        "Estimated Revenue (€)": "{:.0f}".format
+    }) 
+    st.write("But what about old customers?")
+    st.write("Wine lovers don't stick to brands, but do like to stick to a type of wine.")
+    st.write("The following table shows the top 10 wines inside the top 5 most popular wine types.")   
+    st.table(formatted_df2)
+
+    
+
 
 elif view == "Marketing Priorities":
-    st.header("Marketing Budget Prioritization")
-    st.table(marketing_priorities)
-    fig, ax = plt.subplots()
-    ax.scatter(marketing_priorities["Country"], marketing_priorities["Marketing Score"], color='green')
-    plt.xlabel("Country")
-    plt.ylabel("Marketing Score")
-    st.pyplot(fig)
+    st.header("Marketing Budget Prioritization per Country")
+    df = prioritize_country()
+    formatted_df = df.rename(columns={
+        "country": "Country",
+        "count_wines": "Number of Wines", 
+        "count_vintages": "Number of Vintages", 
+        "sum_ratings_count": "Number Reviews",
+        "sum_sales_euro": "Estimated Revenue (€)",
+        "avg_price_bottle": "Avg Price (€)"
+    })
+    formatted_df = formatted_df.style.format({
+        "Avg Price (€)": "{:.0f}".format,
+        "Estimated Revenue (€)": "{:.0f}".format
+    })
+    st.table(formatted_df)
+    """ France is popular and provides a lot of revenue, also it has a good availability of wines and vintages."""
+    """At the same time, Italy also has a good availability of wines and vintages, but the price is cheap compared to France."""
+    """Since France is already so popular, we recommend focusing on Italy to drive the median price up and improve revenue."""
 
 elif view == "Winery Awards":
     st.header("Awards for Best Wineries")
-    st.table(award_best_wineries())
+    df = award_best_wineries()
+    formatted_df = df.rename(columns={
+        "winery_name": "Winery",
+        "num_of_wines": "Number of Wines", 
+        "avg_rating": "Avg Rating", 
+        "total_ratings": "Number Reviews",
+        "avg_price": "Avg Price (€)"
+    })
+    formatted_df = formatted_df.style.format({
+        "Avg Price (€)": "{:.0f}".format,
+        "Avg Rating": "{:.1f}".format
+    })
+    st.table(formatted_df)
 
 elif view == "Keyword Wines":
     st.header("Keyword-Related Wine Selection")
-    st.table(keyword_wines)
+    df = find_wines_by_taste()
+    formatted_df = df.rename(columns={
+        "wine_id": "Id",
+        "wine": "Wine"
+    })
+    """Those are the wines that match all those flavors: 'coffee', 'toast', 'green apple', 'cream' and 'citrus'."""
+    """As we can see, most are Brut Champagne. And therefore we would name this customer cluster as 'Lovers of Brut Champagne'"""
+
+    st.table(formatted_df)
+
 
 elif view == "Top Rated Accessible Wines":
     st.header("Top Rated Wines from Worldwide Grapes")
-    st.table(select_common_grapes_wines())
+    df = select_common_grapes_wines()
+    formatted_df = df.rename(columns={
+        "grape_name": "Grape",
+        "wine_name": "Wine", 
+        "rating": "Rating"
+    })
+    formatted_df = formatted_df.style.format({
+        "Rating": "{:.0f}".format
+    })
+    st.table(formatted_df)
+
 
 elif view == "Country & Vintage Ratings":
     st.header("Country and Vintage Leaderboard")
@@ -117,7 +153,18 @@ elif view == "Country & Vintage Ratings":
     st.write("Vintage Ratings")
     st.table(vintage_ratings)
 
+
 elif view == "VIP Recommendations":
     st.header("Top Picks for a VIP Client")
-    st.table(recommend_cabernet_sauvignon())
+
+    df = recommend_cabernet_sauvignon()
+    formatted_df = df.rename(columns={
+        "wine_name": "Wine", 
+        "ratings_avg": "Average Rating",
+        "ratings_count": "Number reviews"
+    })
+    formatted_df = formatted_df.style.format({
+        "Average Rating": "{:.1f}".format
+    })
+    st.table(formatted_df)
     
--- src/utils/get_results.py
+++ src/utils/get_results.py
@@ -5,11 +5,12 @@ from utils.db import select_query_to_pandas
 
 def highlight_top_wines_1():
     """
-    Highlights 10 wines to increase sales based on specific criteria such as sales trends,
-    ratings, and uniqueness.
+    Q1: Highlights 10 wines to increase sales based on specific criteria such as sales trends,
+    ratings, and uniqueness. 
+    Approach here is by a measure of: popular wines, of good quality and many harvests.
     """
     df_results = select_query_to_pandas("""
-    SELECT name, ROUND(numb_vintages *total_count* avg_rating) AS measure, average_weighted_price, avg_rating, total_count, total_sale, numb_vintages  
+    SELECT id, name, ROUND(numb_vintages *total_count* avg_rating/1000000) AS measure, average_weighted_price, numb_vintages, avg_rating, total_count, total_sale  
     FROM sales_per_wine spw
     ORDER BY measure DESC
     LIMIT 10
@@ -19,25 +20,33 @@ def highlight_top_wines_1():
 
 def highlight_top_wines_2():
     """
-    Highlights 10 wines to increase sales based on specific criteria such as sales trends,
-    ratings, and uniqueness.
+    Q1: Highlights 10 wines to increase sales based on specific criteria such as sales trends,
+    ratings, and uniqueness. 
+    Approach here is getting 5 popular types of wine and showing top 10 popular by reviews on those types.
     """
     df_results = select_query_to_pandas("""
     WITH top_types AS (
-        SELECT fv.fk_wine_id AS wine_id, SUM(fv.ratings_count) as total_reviews, dt.toplists_type_wine as type_wine
+        SELECT 
+            fv.fk_wine_id AS wine_id, 
+            SUM(fv.ratings_count) as total_reviews, 
+            dt.toplists_type_wine as type_wine
         FROM Fact_vintages fv 
-        JOIN Dim_toplists dt ON dt.toplists_id = fv.fk_last_toplist
+        JOIN Dim_toplists dt 
+            ON dt.toplists_id = fv.fk_last_toplist
         GROUP BY fv.fk_wine_id
         ORDER BY total_reviews DESC
     )
     
-    SELECT top_types.wine_id, dw.wine_name, spw.total_count , type_wine, spw.average_weighted_price, spw.numb_vintages, spw.total_sale  
+    SELECT top_types.wine_id, dw.wine_name, type_wine, spw.average_weighted_price, spw.numb_vintages, spw.total_count , spw.total_sale  
     FROM top_types
-    JOIN Dim_wines dw ON dw.wine_id = top_types.wine_id
-    JOIN sales_per_wine spw ON spw.name = dw.wine_name
-    WHERE type_wine IN (SELECT DISTINCT type_wine
-    FROM top_types
-    LIMIT 5) 
+    JOIN Dim_wines dw 
+        ON dw.wine_id = top_types.wine_id
+    JOIN sales_per_wine spw 
+        ON spw.name = dw.wine_name
+    WHERE type_wine 
+        IN (SELECT DISTINCT type_wine
+            FROM top_types
+            LIMIT 5) 
     AND numb_vintages > 5
     ORDER BY total_count DESC
     LIMIT 10
@@ -45,13 +54,28 @@ def highlight_top_wines_2():
     return df_results
 
 
-def prioritize_country(df):
+def prioritize_country():
     """
-    Determines which country to prioritize for marketing based on factors like market potential,
+    Q2: Determines which country to prioritize for marketing based on factors like market potential,
     current sales trends, and budget constraints.
+    Approach: See which countries bring the most revenue, and general metrics for comparing as number of wines, harvests, reviews, avg price.
     """
-    # Logic to be implemented later
-    return pd.DataFrame()  # This will eventually return a DataFrame
+    df_results = select_query_to_pandas("""
+    SELECT 
+        dc.country_name AS country,  
+        COUNT(spw.name) AS count_wines,
+        SUM (spw.numb_vintages) AS count_vintages,
+        SUM(spw.total_count) AS sum_ratings_count, 
+        SUM(spw.total_sale) as sum_sales_euro, 
+        ROUND(AVG (spw.average_weighted_price)) AS avg_price_bottle
+    FROM sales_per_wine spw
+    JOIN Dim_countries dc ON dc.country_code = spw.country 
+    GROUP BY spw.country
+
+    ORDER BY sum_sales_euro DESC
+    LIMIT 5
+    """)
+    return df_results
 
 
 def award_best_wineries():
@@ -60,20 +84,49 @@ def award_best_wineries():
     innovation, and contribution to the wine industry.
     """
     df_results = select_query_to_pandas("""
-    SELECT dwn.name, fw.num_of_wines, fw.avg_rating, fw.total_ratings, fw.avg_price
-    FROM Fact_Wineries fw LEFT JOIN Dim_Winery_Name dwn ON fw.winery = dwn.id 
+    SELECT dwn.winery_name, fw.num_of_wines, fw.avg_rating, fw.total_ratings, fw.avg_price
+    FROM Fact_Wineries fw LEFT JOIN Dim_Wineries dwn ON fw.fk_winery_id = dwn.winery_id 
     ORDER BY fw.overal_score DESC 
     LIMIT 3  
     """)
     return df_results
 
 
-def find_wines_by_taste(df, keywords, min_users=10):
+def find_wines_by_taste():
     """
-    Finds all wines that match specific taste keywords confirmed by at least a given number of users.
+    Q4: Finds all wines that match specific taste keywords confirmed by at least a given number of users.
+    Approach: find all wines that match all the listed flavors.
     """
-    # Logic to be implemented later
-    return pd.DataFrame()  # This will eventually return a DataFrame
+    df_results = select_query_to_pandas("""
+    WITH filtered_fact_keywords_wine AS (
+        SELECT 
+            DISTINCT fkw.keyword_id, 
+            dk.keywords_name AS key_word, 
+            fkw.fk_wine_id AS wine_id, 
+            fkw.count_keyword,
+            dw.wine_name AS wine        
+        FROM Fact_keywords_wine fkw 
+        JOIN Dim_keywords dk 
+            ON dk.keywords_id = fkw.keyword_id 
+        JOIN Dim_wines dw 
+            ON dw.wine_id = fkw.fk_wine_id 
+        WHERE dk.keywords_name IN ('coffee', 'toast', 'green apple', 'cream', 'citrus')
+            AND fkw.count_keyword >=10
+    ),
+    match_all AS (
+        SELECT 
+            wine_id, 
+            wine, COUNT(key_word) AS n_matched_flavors, 
+            GROUP_CONCAT(key_word, ', ') AS matched_flavors
+        FROM filtered_fact_keywords_wine
+        GROUP BY wine_id
+        HAVING n_matched_flavors = 5
+        ORDER BY n_matched_flavors DESC, wine ASC
+    )
+    SELECT wine_id, wine
+    FROM match_all
+    """)
+    return df_results 
 
 
 # noinspection SqlDialectInspection,SqlNoDataSourceInspection
