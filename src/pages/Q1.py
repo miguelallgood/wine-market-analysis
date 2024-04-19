74fdfb7a594c559f27d4b6ec859b12587a2f16fe
Nachtje <datanerd07@gmail.com>
2024-04-19 11:17:51 +0200

    Q1 first test with pages

--- src/pages/Q1.py
+++ src/pages/Q1.py
@@ -1,7 +1,9 @@
 import streamlit as st
-import requests
 import pandas as pd
-import os
+import matplotlib.pyplot as plt
+
+from utils.get_results import award_best_wineries, recommend_cabernet_sauvignon, select_common_grapes_wines, \
+    highlight_top_wines_1, highlight_top_wines_2
 
 # Configure the page
 st.set_page_config(
@@ -12,5 +14,15 @@ st.set_page_config(
 #####Sidebar Start#####
 
 # Add a sidebar
-st.sidebar.markdown("### **Q1**")
+st.sidebar.markdown("### **Top10 Wines**")
+
+df1 = highlight_top_wines_1()
+df2 = highlight_top_wines_2()
+formatted_df1 = df1.style.format({
+    "measure": "{:.0f}".format,
+    "avg_rating": "{:.1f}".format,
+    "average_weighted_price": "{:.0f}".format,
+})
+st.table(formatted_df1)
+st.table(df2)
 
